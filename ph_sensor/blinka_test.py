e8132002a77379410d1e60129beab82010192939
Jerome Swannack <jeromeswannack@gmail.com>
2023-03-26 18:52:04 +1300

    Added blinka deps and test script

---
+++ ph_sensor/blinka_test.py
@@ -0,0 +1,20 @@
+import time
+import board
+import busio
+from adafruit_ads1x15.analog_in import AnalogIn
+from adafruit_ads1x15.ads1x15 import ADS1115
+
+# Initialize the I2C bus.
+i2c = busio.I2C(board.SCL, board.SDA)
+
+# Initialize the ADC object using the ADS1115 class.
+adc = ADS1115(i2c)
+
+# Create a single-ended input on channel A0.
+chan = AnalogIn(adc, ADS1115.P0)
+
+# Continuously print the voltage value.
+while True:
+    print("Channel A0 Voltage: {:.2f} V".format(chan.voltage))
+    time.sleep(1)
+
